43d8b327721add19ad46b4cbf3ea5898889e88d6
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2022-06-28 00:55:48 +0300

    added parallel stream solution

--- src/main/java/ru/digitalhabbits/homework2/impl/AsyncFileLetterCounter.java
+++ src/main/java/ru/digitalhabbits/homework2/impl/AsyncFileLetterCounter.java
@@ -7,8 +7,8 @@ import ru.digitalhabbits.homework2.LetterCountMerger;
 import ru.digitalhabbits.homework2.LetterCounter;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,11 +20,20 @@ public class AsyncFileLetterCounter implements FileLetterCounter {
     private final FileReader fileReader = new FileReaderImpl();
     private final LetterCounter letterCounter = new LetterCounterImpl();
     private final LetterCountMerger letterCountMerger = new LetterCountMergerImpl();
-    private Map<Character, Long> resultMap = new ConcurrentHashMap<>();
+    private Map<Character, Long> resultMap;
     private final ExecutorService executorService = Executors.newFixedThreadPool(8);
 
     @Override
     public Map<Character, Long> count(File input) {
+
+        // так тоже работает
+//        return fileReader.readLines(input).parallel()
+//                .map(letterCounter::count)
+//                .reduce(letterCountMerger::merge)
+//                .orElse(Collections.emptyMap());
+
+        resultMap = new HashMap<>();
+
         fileReader.readLines(input)
                 .forEach(line -> {
                     log.info("Read line from file: {}", line);
@@ -32,6 +41,7 @@ public class AsyncFileLetterCounter implements FileLetterCounter {
                             .thenAcceptAsync(map -> resultMap = letterCountMerger.merge(resultMap, map), executorService)
                             .join();
                 });
+
         return resultMap;
     }
 
